6d5c403927eb4c4be5ee21e44ea1bd17b7c38ad8
Emojigit <55009343+Emojigit@users.noreply.github.com>
2021-07-16 15:19:53 +0800

    init

---
+++ init.lua
@@ -0,0 +1,43 @@
+local HUDID = nil
+local lastupdate = 0
+local lastHP = 0
+
+minetest.register_globalstep(function(dtime)
+  if not (minetest.localplayer) then return end
+  HUDID = HUDID or minetest.localplayer:hud_add({
+    hud_elem_type = "text",
+    position      = {x = 1, y = 0},
+    scale         = {x = 100, y = 100},
+    text          = "",
+    number        = 0xFFFFFF,
+    offset        = {x = -10, y = 25},
+    alignment     = {x = -1, y = 3}
+  })
+  if not HUDID then
+    error("Show damage mod failed on HUD element creating!")
+  end
+  lastupdate = lastupdate - dtime
+  if lastupdate < 0 and minetest.localplayer:hud_get(HUDID).text ~= "" then
+    minetest.localplayer:hud_change(HUDID, "text", "")
+  end
+end)
+
+minetest.register_on_hp_modification(function(hp)
+  local hpmod = hp - lastHP
+  lastHP = hp
+  if not HUDID then
+    return
+  end
+  local hpstr = tostring(hpmod)
+  local color = 0x00FF00
+  if hpmod < 0 then
+    color = 0xFF0000
+  else
+    hpstr = "+" .. hpstr
+  end
+  lastupdate = 5 -- Reset after 5 seconds
+  minetest.localplayer:hud_change(HUDID, "number", color)
+  minetest.localplayer:hud_change(HUDID, "text", hpstr)
+end)
+
+minetest.log("info","[Show Damage CSM] OK")
